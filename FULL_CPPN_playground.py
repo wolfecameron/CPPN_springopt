1c6fb92bcbd135071318aa55f912588a31f1b091
wolfecameron <wolfe.cameron@utexas.edu>
2018-06-14 21:54:02 -0500

    finished implementing initial playground GUI

--- FULL_CPPN_playground.py
+++ FULL_CPPN_playground.py
@@ -28,7 +28,8 @@ def init_playground(genotype, num_x, num_y):
 
 	# initiate figure and subplot needed for graphing
 	f = Figure(figsize=(10, 10), dpi=100)
-	subp = f.add_subplot(211)
+	subp_1 = f.add_subplot(211)
+	subp_2 = f.add_subplot(212)
 
 	# initiate GUI
 	master = tk.Tk()
@@ -36,14 +37,14 @@ def init_playground(genotype, num_x, num_y):
 
 	# create graphs of genotype and phenotype
 	
-	graph_genotype_GUI(genotype, f)
+	graph_genotype_GUI(genotype, subp_2)
 	# get outputs for current genotype
 	norm_in = getNormalizedInputs(num_x, num_y)
 	outputs = []
 	for ins in norm_in:
 		outputs.append(genotype.getOutput(ins)[0])
 	outputs_np = np.array(outputs, copy=True)
-	subp.imshow(np.reshape(outputs_np, (num_x, num_y)), cmap='Greys')
+	subp_1.imshow(np.reshape(outputs_np, (num_x, num_y)), cmap='Greys')
 
 	# create canvas for GUI
 	canvas = FigureCanvasTkAgg(f, master)
@@ -51,23 +52,23 @@ def init_playground(genotype, num_x, num_y):
 	canvas.get_tk_widget().pack()
 
 	# create labels for text entry and text entry
-	tk.Label(master, text="Innovation Number").pack()
+	tk.Label(master, text="Innovation Number").pack(pady=(10,0))
 	tk.Label(master, text="New Weight").pack()
 	innov_e = tk.Entry(master)
 	weight_e = tk.Entry(master)
-	innov_e.pack()
+	innov_e.pack(pady=(10,0))
 	weight_e.pack()
 
 	# create buttons for exit and entry
-	tk.Button(master, text="Exit", command=master.quit).pack()
-	tk.Button(master, text="Submit", command=(lambda: update_CPPN(genotype, innov_e, weight_e))).pack()
+	tk.Button(master, text="Submit", command=(lambda: update_CPPN(genotype, innov_e, weight_e, subp_1, subp_2, norm_in, num_x, num_y, canvas, f))).pack()
+	tk.Button(master, text="Exit", command=master.quit).pack(pady=(20,10))
 
 	# initiate main loop for GUI
 	tk.mainloop()
 
 
 
-def update_CPPN(E1, E2):
+def update_CPPN(genotype, E1, E2, subp_1, subp_2, norm_in, num_x, num_y, canvas, f):
 	"""Function for taking values from the user 
 	and using them to update weight values in the 
 	CPPN - user chooses new weight for a certain
@@ -77,9 +78,7 @@ def update_CPPN(E1, E2):
 	# get values from fields
 	innov_num = E1.get()
 	new_weight = E2.get()
-	print(innov_num)
-	print(new_weight)
-	input()
+
 
 	# search through connections to find the correct
 	# weight to change
@@ -87,8 +86,27 @@ def update_CPPN(E1, E2):
 		if(con.getInnovationNumber() == int(innov_num)):
 			con.setWeight(float(new_weight))
 
+	# clear both subplots so new graphs can be placed into the GUI
+	subp_1.clear()
+	subp_2.clear()
 
-def graph_genotype_GUI(genotype, f):
+	# create graphs again and put graphs onto them
+	outputs = []
+	for ins in norm_in:
+		outputs.append(genotype.getOutput(ins)[0])
+	outputs_np = np.array(outputs, copy=True)
+	subp_1.imshow(np.reshape(outputs_np, (num_x, num_y)), cmap='Greys')
+
+	graph_genotype_GUI(genotype, subp_2)
+
+	canvas.show()
+	canvas.get_tk_widget().pack()
+
+
+
+
+
+def graph_genotype_GUI(genotype, subp):
 	"""Function used to graph genotype of CPPN, function is same as
 	the one inside of the CPPN structure function but can be added to
 	a subplot of Figure f to be included in the GUI display for the
@@ -102,9 +120,6 @@ def graph_genotype_GUI(genotype, f):
 	# create the networkx graph and associated node position for genotype
 	graph = genotype.gen_networkx_graph()
 	pos = genotype.gen_positions_for_networkx(graph)
-
-	# create matplotlib figure used for graph
-	subp = f.add_subplot(212)
 	
 	# add all nodes into graph with colors
 	for node in genotype.nodes:
