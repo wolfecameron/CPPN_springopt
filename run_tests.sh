94c6f11f7a9ffbffb126fe10f6e9a88aed5f476c
Cameron Wolfe <wolfe.cameron@utexas.edu>
2018-06-30 19:15:59 -0500

    simple shell script change

--- run_tests.sh
+++ run_tests.sh
@@ -2,7 +2,7 @@
 # set variable for seed number to be used
 SEED=3
 # create a for loop that calls script multiple times
-for i in "bez_ex.png" "bez_ex" "loop_ex.png" "pacman_ex.png" 
+for i in "bez_ex.png" "bez_ex.png" "loop_ex.png" "pacman_ex.png" 
 do
 	python -m scoop -n 8 FULL_CPPN_deapea.py $i $SEED
 done	
