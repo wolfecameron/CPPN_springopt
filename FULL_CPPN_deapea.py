7a7b94c23e582d85c6bc97f18a5d9756b3567529
Cameron Wolfe <wolfe.cameron@utexas.edu>
2018-07-04 21:55:27 -0500

    edited shell script for a new test

--- FULL_CPPN_deapea.py
+++ FULL_CPPN_deapea.py
@@ -62,7 +62,7 @@ LAST_NUM_SPECIES = -1
 # the following is the minimum proportion of material a solution must use 
 # to not be penalized
 MATERIAL_PENALIZATION_THRESHOLD = .1
-MATERIAL_UNPRESENT_PENALIZATION = 2.5
+MATERIAL_UNPRESENT_PENALIZATION = 2
 
 # sets global parameters for 2D structure being created by CPPN, generates inputs
 NUM_X = 50
@@ -313,7 +313,7 @@ def main(nGen, weightMutpb, nodeMutpb, conMutpb, cxPb, actMutpb, thresh, alpha, 
 # runs the main evolutionary loop if this file is ran from terminal
 if __name__ == '__main__':
 	'''
-	pop_tup = pickle.load(open('/home/wolfecameron/Desktop/CPPN_pop_result/CPPN_sensitivity_test_15.txt', 'rb'))
+	pop_tup = pickle.load(open('/home/wolfecameron/Desktop/CPPN_pop_result/CPPN_delete_con_test_1.txt', 'rb'))
 	pop = pop_tup[0]
 	for individual in pop:
 		org = Genotype(2,1)
@@ -349,5 +349,5 @@ if __name__ == '__main__':
 	file_name = get_file_name("/home/crwolfe/Documents/CPPN_test_env/CPPN_pop_result", "CPPN_delete_con_test_")
 
 	save_population(finalPop, SEED, file_name)
-	
+
 
--- run_tests.sh
+++ run_tests.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
-# set variable for seed number to be used
+# set variables for all terminal arguments
 SEED=1
+WEIGHT=30
+NODE=3
+CON=25
+ACT=5
+CROSS=10
 
 # create a for loop that calls script multiple times
 for i in "heart_ex.png" "pacman_ex.png" "spring10.png"
 do
-	python -m scoop -n 8 FULL_CPPN_deapea.py $i $SEED
+	# run twice to get more chances at each picture
+	python -m scoop -n 8 FULL_CPPN_deapea.py $i $SEED $WEIGHT $NODE $CON $ACT $CROSS
+	python -m scoop -n 8 FULL_CPPN_deapea.py $i $SEED $WEIGHT $NODE $CON $ACT $CROSS
 done	
